aa1f391df7ea43243f9f98a0f1062a3466920e51
Thibaut Deloffre <t.deloffre@alphalink.fr>
2012-12-26 16:38:33 +0100

    Test update.

--- test/test_conf.rb
+++ test/test_conf.rb
@@ -5,18 +5,18 @@ require 'bleetz/object'
 class TestConf < Test::Unit::TestCase
 
   def test_fail_action
-      begin
-        load Dir.pwd + '/test/files/fail_action'
-      rescue Exception => e
-        assert_equal "Main configuration function, you cannot call 'action' in 'action'.", e.message
-      end
+    begin
+      load Dir.pwd + '/test/files/fail_action'
+    rescue Exception => e
+      assert_equal "Conf error: RuntimeError: Main configuration function, you cannot call 'action' in 'action", e.message
+    end
   end
 
   def test_fail_set
     begin
       load Dir.pwd + '/test/files/fail_set'
     rescue Exception => e
-      assert_equal "Main configuration function, you cannot call 'set' in 'action'.", e.message
+      assert_equal "Conf error: RuntimeError: Main configuration function, you cannot call 'set' in 'action'.", e.message
     end
   end
 
