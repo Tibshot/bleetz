fe4878f23d586f2f99313ced1feed24e02072000
Thibaut Deloffre <t.deloffre@alphalink.fr>
2013-01-02 12:47:49 +0100

    DRY BABY DRY ! (conf.rb)

--- lib/bleetz/conf.rb
+++ lib/bleetz/conf.rb
@@ -28,16 +28,7 @@ module Conf
 
   def action(action, desc = "")
     check_call(:action)
-    @cmds = []
-    begin
-      yield
-    rescue Exception => e
-      if e.class.eql? RuntimeError
-        raise BleetzException.new(e.message)
-      else
-        raise BleetzException.new("#{e.class}: #{e.message} in #{e.backtrace[0]}")
-      end
-    end
+    load_conf { yield }
     h = { action.to_sym => @cmds }
     t = { action.to_sym => desc.to_s }
     @@actions = @@actions.merge(h)
@@ -46,16 +37,7 @@ module Conf
 
   def before(action)
     check_call(:before)
-    @before = []
-    begin
-      yield
-    rescue Exception => e
-      if e.class.eql? RuntimeError
-        raise BleetzException.new(e.message)
-      else
-        raise BleetzException.new("#{e.class}: #{e.message} in #{e.backtrace[0]}")
-      end
-    end
+    load_conf { yield }
     h = { action.to_sym => @before }
     if @@before[action.to_sym].nil?
       @@before = @@before.merge(h)
@@ -65,17 +47,8 @@ module Conf
   end
 
   def after(action)
-    @after = []
     check_call(:after)
-    begin
-      yield
-    rescue Exception => e
-      if e.class.eql? RuntimeError
-        raise BleetzException.new(e.message)
-      else
-        raise BleetzException.new("#{e.class}: #{e.message} in #{e.backtrace[0]}")
-      end
-    end
+    load_conf { yield }
     h = { action.to_sym => @after }
     if @@before[action.to_sym].nil?
       @@after = @@after.merge(h)
@@ -110,14 +83,28 @@ module Conf
   private
 
   def check_call(func)
-    parent_call = caller[2][/`([^']*)'/, 1]
     if MAIN_CALLS[:func].include?(func.to_s)
+      parent_call = caller[2][/`([^']*)'/, 1]
       unless MAIN_CALLS[:from].include?(parent_call)
         raise "#{caller[1].split(" ")[0]} '#{func}'. Main functions cannot be called in functions."
       end
     else
+      parent_call = caller[4][/`([^']*)'/, 1]
       unless SUB_CALLS[func].include?(parent_call)
         raise "#{caller[1].split(" ")[0]} #{SUB_CALLS[:error][func]}"
+      end
+    end
+  end
+
+  def load_conf
+    @after = @cmds = @before = []
+    begin
+      yield
+    rescue Exception => e
+      if e.class.eql? RuntimeError
+        raise BleetzException.new(e.message)
+      else
+        raise BleetzException.new("#{e.class}: #{e.message} in #{e.backtrace[0]}")
       end
     end
   end
